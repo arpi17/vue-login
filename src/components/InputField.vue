06ae2b806931010a9a61613baca78aeddb0b1d63
Arpad Illyes <arpadillyes93@gmail.com>
2019-05-28 12:47:36 +0200

    Add login fields

---
+++ src/components/InputField.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="form-group">
+    <label :for="label">{{ label }}</label>
+    <input
+      :id="label"
+      class="form-control"
+      :class="{'is-invalid': error}"
+      :value="value"
+      :type="type"
+      :placeholder="placeholder"
+      @input="$emit('input', $event.target.value)"
+    >
+    <small v-show="error" :class="{'invalid-feedback': error}">{{error}}</small>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InputField",
+  props: {
+    value: String,
+    type: String,
+    label: String,
+    error: String,
+    placeholder: String
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="card" style="width: 240px">
+      <div class="card-body">
+        <form @submit.prevent="$emit">
+          <InputField
+            v-model="username"
+            type="text"
+            placeholder="Enter username"
+            label="Username"
+            ref="autofocus"
+          />
+          <InputField
+            v-model="password"
+            type="password"
+            placeholder="Enter password"
+            label="Password"
+            error="Incorrect password"
+          />
+          <button class="btn btn-primary">Sign in</button>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import InputField from "@/components/InputField.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    InputField
+  },
+  data() {
+    return {
+      username: "",
+      password: ""
+    };
+  },
+  mounted() {
+    this.$refs.autofocus.$el.querySelector("input").focus();
   }
 };
 </script>
