8c2deb7f0732bf07562cf1e3954580235f6a5732
Arpad Illyes <arpadillyes93@gmail.com>
2019-05-31 02:47:27 +0200

    Revert

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -4,9 +4,7 @@
       <h1 class="my-4">Create an account</h1>
       <div class="card mx-auto" style="width: 280px">
         <div class="card-body">
-          <form
-            @submit.prevent="registerUser({ username, password, password2 })"
-          >
+          <form @submit.prevent="registerUser({ username, password, password2 })">
             <InputField
               v-model="username"
               type="text"
@@ -42,7 +40,6 @@
 </template>
 
 <script>
-import axios from "axios";
 import { mapGetters, mapMutations, mapActions } from "vuex";
 import InputField from "@/components/InputField.vue";
 
