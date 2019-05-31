f64c536d55a4d28d7ad83c11d03c5f72221f6c2f
Arpad Illyes <arpadillyes93@gmail.com>
2019-05-31 02:10:16 +0200

    Fix typo

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keys = require('./config/keys');
+const path = require('path');
 
 const app = express();
 
@@ -18,13 +19,13 @@ const User = require('./UserModel');
 const validateRegistration = require('./validators/register');
 const validateLogin = require('./validators/login');
 
+// Connect to database
 mongoose
   .connect(require('./config/keys').mongoURI, { useNewUrlParser: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
 // Routes
-
 app.get('/users', (req, res) => {
   User.find()
     .then(users => res.json(users))
@@ -76,6 +77,14 @@ app.post('/users/login', validateLogin, (req, res) => {
     });
   });
 });
+
+// Serve static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('dist'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+  });
+}
 
 const PORT = 5000 || process.env.PORT;
 
